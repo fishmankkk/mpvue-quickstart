029b68446fe78d1c64e77c25d735b097c3c192be
XiaoKeng Huang <609745278@qq.com>
2018-07-26 10:25:32 +0800

    fixed

--- src/helper/mp-config.js
+++ src/helper/mp-config.js
@@ -1,9 +1,6 @@
 export default Object.assign({}, {
-  path: 'http://172.16.140.65:8040/wechat-api', // 杜建雄
-  // path: 'http://127.0.0.1:8089/api', // mockjs
+  path: 'http://127.0.0.1:8089/api', // mockjs
   codeVersion: '2.3.1.9', // 代码版本 用于审核
-  reportAnalysisBaseUrl: 'https://enterapp.sunlands.com/datacenter_app/dataService' // 数据上报的地址
 }, process.env.NODE_ENV === 'production' ? {
-  path: 'https://wechatapi.sunlands.com/wechat-api',
-  reportAnalysisBaseUrl: 'https://datacenter.sunlands.com/datacenter_app/dataService'
+  path: 'http://127.0.0.1:8089/api'
 } : {})
